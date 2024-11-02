fd2fdd098068af1d7e266c7d25174ee7b603ddd9
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2024-11-02 17:19:19 +0900

    [move]: day,month viewConatiner를 container패키지로 이동

--- Timely/src/main/java/com/dongsu/timely/presentation/ui/main/calendar/home/container/MonthViewContainer.kt
+++ Timely/src/main/java/com/dongsu/timely/presentation/ui/main/calendar/home/container/MonthViewContainer.kt
@@ -1,4 +1,4 @@
-package com.dongsu.timely.presentation.ui.main.calendar.home
+package com.dongsu.timely.presentation.ui.main.calendar.home.container
 
 import android.view.View
 import android.view.ViewGroup
