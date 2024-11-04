360efb2df60be5f4fb7bfb63387d97222332babb
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2024-11-04 20:40:04 +0900

    [rename]: module 파일 이름 변경 - RetrofitRESTModule -> NetworkingModule - RetrofitModule -> ServiceModule

--- data/src/main/java/com/dongsu/timely/data/di/ServiceModule.kt
+++ data/src/main/java/com/dongsu/timely/data/di/ServiceModule.kt
@@ -1,8 +1,6 @@
 package com.dongsu.timely.data.di
 
 import com.dongsu.timely.data.remote.service.TmapService
-import com.dongsu.timely.data.repository.LocationRepositoryImpl
-import com.dongsu.timely.domain.repository.LocationRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitModule {
+object ServiceModule {
 
     @Provides
     @Singleton
