3fe2f305b20f701cab2498b209b194cfecb5d9df
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2024-11-04 18:07:04 +0900

    [feat]: 바텀네비게이션뷰 보일 곳 설정하기

--- Timely/src/main/java/com/dongsu/timely/presentation/ui/main/TimelyActivity.kt
+++ Timely/src/main/java/com/dongsu/timely/presentation/ui/main/TimelyActivity.kt
@@ -1,9 +1,9 @@
 package com.dongsu.timely.presentation.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.dongsu.timely.R
@@ -12,22 +12,28 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TimelyActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityTimelyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e("TimelyActivity", "onCreate: ")
+
         binding = ActivityTimelyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setupBottomNavigation()
+    }
+    private fun setupBottomNavigation() {
         val navController = (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController
         binding.bottomNavigation.setupWithNavController(navController)
 
+        setBottomNavigationVisibility(navController)
     }
-    fun hideBottomNavigation() {
-        binding.bottomNavigation.visibility = View.GONE
-    }
-
-    fun showBottomNavigation() {
-        binding.bottomNavigation.visibility = View.VISIBLE
+    private fun setBottomNavigationVisibility(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.visibility = when (destination.id) {
+                R.id.calendarFragment, R.id.groupListFragment, R.id.profileFragment, R.id.groupDateFragment, R.id.groupLocationFragment,R.id.groupManagementFragment -> View.VISIBLE
+                else -> View.GONE
+            }
+        }
     }
 }
