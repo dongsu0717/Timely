a33e0c62e5ba179b13529a268fd6e1434439c50d
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2024-11-06 00:20:31 +0900

    [refactor]: TimelyResult의 No -> Empty 변경

--- common/src/main/java/com/dongsu/timely/common/TimelyResult.kt
+++ common/src/main/java/com/dongsu/timely/common/TimelyResult.kt
@@ -1,7 +1,7 @@
 package com.dongsu.timely.common
 
 sealed class TimelyResult<out T> {
-    data object No : TimelyResult<Nothing>()
+    data object Empty : TimelyResult<Nothing>()
     data object Loading : TimelyResult<Nothing>()
     data class Success<T>(val resultData: T) : TimelyResult<T>()
     data class Error(val exception: Throwable) : TimelyResult<Nothing>()
