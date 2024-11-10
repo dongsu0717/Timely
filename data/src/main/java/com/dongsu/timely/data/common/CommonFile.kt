2968caf1b299be614cd16ed577fd97f1a801f091
Dongsu Lee <dongsu0717@nate.com>
2024-11-11 00:00:29 +0900

    [feat]: 통신할 URL추가

--- data/src/main/java/com/dongsu/timely/data/common/CommonFile.kt
+++ data/src/main/java/com/dongsu/timely/data/common/CommonFile.kt
@@ -2,6 +2,7 @@ package com.dongsu.timely.data.common
 
 import android.content.Context
 import android.widget.Toast
+import com.dongsu.data.BuildConfig.TIMELY_HOST_URL
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Singleton
@@ -22,4 +23,23 @@ const val TMAP_PACKAGE_NAME = "com.skt.tmap.ku"
 const val TMAP_ROUTE_URL = "tmap://route?goalname=%s&goalx=%s&goaly=%s&startx=%s&starty=%s"
 const val TMAP_MARKET_URL = "market://details?id=%s"
 
+//TIMELY_URL
+const val PROTOCOL = "http"
+const val HOST = TIMELY_HOST_URL
+const val PORT = 8080
+const val API_VERSION = "v1"
+const val ADDRESS_PORT = "$HOST:$PORT"
+const val TIMELY_BASE_URL = "$PROTOCOL://$ADDRESS_PORT/"
+
+//account
+const val LOGIN_TOKEN = "auth/mobile/kakao"
+const val USER_INFO = "$API_VERSION/users/me"
+const val REISSUED_ACCESS_TOKEN = "auth/token"
+const val REISSUED_ALL_TOKEN = "auth/token/refresh"
+
+//group
+const val GROUP = "$API_VERSION/groups"
+const val GROUP_INFO = "$API_VERSION/groups/{groupId}"
+const val INVITE_GROUP = "$API_VERSION/groups/{groupId}/invite-code"
+const val JOIN_GROUP = "$API_VERSION/groups/signup/{inviteCode)"
 
