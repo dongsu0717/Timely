60260fea565eba9ae5ef6675b20e858d1530bcb5
Dongsu Lee <dongsu0717@nate.com>
2024-11-11 15:14:44 +0900

    [refactor]: SharedPreferences에서 token가져오는 함수 suspend제거 - 비동기로 안해도 SharedPreferences작업은 문제 없다고 판단

--- data/src/main/java/com/dongsu/timely/data/datasource/UserLocalDatasource.kt
+++ data/src/main/java/com/dongsu/timely/data/datasource/UserLocalDatasource.kt
@@ -3,7 +3,7 @@ package com.dongsu.timely.data.datasource
 import com.dongsu.timely.common.TimelyResult
 
 interface UserLocalDatasource {
-    suspend fun getToken()
+    fun getToken():String
     suspend fun saveLoginStatus(accessToken: String, refreshToken: String)
     suspend fun isLoggedIn(): TimelyResult<Boolean>
 }
