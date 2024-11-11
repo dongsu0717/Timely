b46e795d5673ac4654a94474e73f33711843689a
Dongsu Lee <dongsu0717@nate.com>
2024-11-11 15:49:50 +0900

    [refactor]:UserResponse에  @SerializedName추가

--- data/src/main/java/com/dongsu/timely/data/remote/dto/response/UserResponse.kt
+++ data/src/main/java/com/dongsu/timely/data/remote/dto/response/UserResponse.kt
@@ -1,7 +1,12 @@
 package com.dongsu.timely.data.remote.dto.response
 
+import com.google.gson.annotations.SerializedName
+
 data class UserResponse(
+    @SerializedName("userId")
     val userId: Int,
+    @SerializedName("email")
     val email: String?,
+    @SerializedName("nickname")
     val nickname: String
 )
