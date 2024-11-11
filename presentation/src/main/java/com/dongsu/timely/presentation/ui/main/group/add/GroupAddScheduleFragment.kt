5b1732126fe62b57cadfc31363f4180b1188f519
Dongsu Lee <dongsu0717@nate.com>
2024-11-11 20:14:36 +0900

    [rename]: group.schedule 패키지 -> group.add 패키지로 이름 변경 - 그룹 스케줄 추가하는 부분임

--- presentation/src/main/java/com/dongsu/timely/presentation/ui/main/group/add/GroupAddScheduleFragment.kt
+++ presentation/src/main/java/com/dongsu/timely/presentation/ui/main/group/add/GroupAddScheduleFragment.kt
@@ -1,4 +1,4 @@
-package com.dongsu.timely.presentation.ui.main.group.schedule
+package com.dongsu.timely.presentation.ui.main.group.add
 
 import com.dongsu.presentation.databinding.FragmentGroupAddScheduleBinding
 import com.dongsu.timely.presentation.common.BaseFragment
