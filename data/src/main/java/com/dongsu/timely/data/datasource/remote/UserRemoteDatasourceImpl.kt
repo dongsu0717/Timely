84ef75ba1212bcbd8c2abc442fc94ff1a700c0f4
Dongsu Lee <dongsu0717@nate.com>
2024-11-13 01:18:31 +0900

    [refactor]: 카카오 로그인성공시 서버에서 token받아서 sharedPrferences에 저장하는 부분이 remoteDatasource에 있어서 이 부분 수정

--- data/src/main/java/com/dongsu/timely/data/datasource/remote/UserRemoteDatasourceImpl.kt
+++ data/src/main/java/com/dongsu/timely/data/datasource/remote/UserRemoteDatasourceImpl.kt
@@ -1,22 +1,22 @@
 package com.dongsu.timely.data.datasource.remote
 
 import android.util.Log
-import com.dongsu.timely.data.datasource.local.UserLocalDatasource
 import com.dongsu.timely.data.mapper.UserMapper
 import com.dongsu.timely.data.remote.api.LoginService
+import com.dongsu.timely.domain.model.Token
 import javax.inject.Inject
 
 class UserRemoteDatasourceImpl @Inject constructor(
-    private val loginService: LoginService,
-    private val userLocalDatasource: UserLocalDatasource
+    private val loginService: LoginService
 ): UserRemoteDatasource {
 
-    override suspend fun sendToken(token: String) {
+    override suspend fun sendToken(token: String): Token {
         val response = loginService.sendToken(sendTokenRequest = UserMapper.toDto(token))
         Log.e("서버에 카카오토큰 저장 성공","${response.body()?.accessToken}")
-        response.body()?.let { body ->
-            Log.e("서버에서 accessToken받기", "${body.accessToken}")
-            userLocalDatasource.saveLoginStatus(body.accessToken, body.refreshToken)
-        }
+        return Token(accessToken = response.body()?.accessToken, refreshToken = response.body()?.refreshToken)
+    }
+
+    override suspend fun sendFCMToken(token: String) {
+
     }
 }
--- data/src/main/java/com/dongsu/timely/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/dongsu/timely/data/repository/UserRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.dongsu.timely.data.repository
 
+import android.util.Log
 import com.dongsu.timely.common.TimelyResult
 import com.dongsu.timely.data.datasource.local.UserLocalDatasource
 import com.dongsu.timely.data.datasource.remote.UserRemoteDatasource
@@ -10,8 +11,13 @@ class UserRepositoryImpl @Inject constructor(
     private val userLocalDatasource: UserLocalDatasource,
     private val userRemoteDatasource: UserRemoteDatasource
 ): UserRepository {
-    override suspend fun sendToken(token: String)
-    = userRemoteDatasource.sendToken(token)
+    override suspend fun sendToken(token: String) {
+        val token = userRemoteDatasource.sendToken(token)
+        if(token.accessToken != null && token.refreshToken != null){
+            Log.e("서버에서 accessToken받기", "${token.accessToken}")
+            userLocalDatasource.saveLoginStatus(token.accessToken.toString(), token.refreshToken.toString())
+        }
+    }
 
     override suspend fun saveStatus(accessToken: String, refreshToken: String)
     = userLocalDatasource.saveLoginStatus(accessToken,refreshToken)
