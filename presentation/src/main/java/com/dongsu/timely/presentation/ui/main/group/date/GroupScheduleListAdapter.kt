8663cb6dc4999b5224186ca3806bb5f87faab4bc
Dongsu Lee <dongsu0717@nate.com>
2024-11-14 03:45:17 +0900

    [fix]: 그룹 스케줄 리스트 알람없는건 알람없는 이미지로 변경, 및 장소없으면 표기x

--- presentation/src/main/java/com/dongsu/timely/presentation/ui/main/group/date/GroupScheduleListAdapter.kt
+++ presentation/src/main/java/com/dongsu/timely/presentation/ui/main/group/date/GroupScheduleListAdapter.kt
@@ -5,24 +5,27 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.dongsu.presentation.R
 import com.dongsu.presentation.databinding.ItemCardGroupScheduleBinding
 import com.dongsu.timely.domain.model.GroupScheduleInfo
+import com.dongsu.timely.presentation.common.formatDateTime
 
 
 class GroupScheduleListAdapter(
     private val onCheckBoxClick: (GroupScheduleInfo, Boolean) -> Unit
-) : ListAdapter<GroupScheduleInfo, GroupScheduleListAdapter.GroupScheduleListViewHolder>(GroupDiffCallback()) {
+) : ListAdapter<GroupScheduleInfo, GroupScheduleListAdapter.GroupScheduleListViewHolder>(
+    GroupDiffCallback()
+) {
 
-    class GroupScheduleListViewHolder(private val binding: ItemCardGroupScheduleBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(groupSchedule: GroupScheduleInfo, onCheckBoxClick: (GroupScheduleInfo, Boolean) -> Unit){
-            with(binding){
-                tvGroupName.text = groupSchedule.title
-                tvGroupPlace.text = groupSchedule.location
-                tvGroupTime.text = groupSchedule.startTime
-                checkBoxParticipation.isChecked = groupSchedule.isAlarmEnabled
-                checkBoxParticipation.setOnCheckedChangeListener { buttonView, isChecked ->
-                    onCheckBoxClick(groupSchedule, isChecked)
-                }
+    inner class GroupScheduleListViewHolder(private val binding: ItemCardGroupScheduleBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            groupSchedule: GroupScheduleInfo,
+            onCheckBoxClick: (GroupScheduleInfo, Boolean) -> Unit
+        ) {
+            setCardView(binding, groupSchedule)
+            binding.checkBoxParticipation.setOnCheckedChangeListener { buttonView, isChecked ->
+                onCheckBoxClick(groupSchedule, isChecked)
             }
         }
     }
@@ -39,13 +42,32 @@ class GroupScheduleListAdapter(
     }
 
     class GroupDiffCallback : DiffUtil.ItemCallback<GroupScheduleInfo>() {
-        override fun areItemsTheSame(oldItem: GroupScheduleInfo, newItem: GroupScheduleInfo): Boolean {
+        override fun areItemsTheSame(
+            oldItem: GroupScheduleInfo,
+            newItem: GroupScheduleInfo
+        ): Boolean {
             return oldItem.scheduleId == newItem.scheduleId
         }
 
-        override fun areContentsTheSame(oldItem: GroupScheduleInfo, newItem: GroupScheduleInfo): Boolean {
+        override fun areContentsTheSame(
+            oldItem: GroupScheduleInfo,
+            newItem: GroupScheduleInfo
+        ): Boolean {
             return oldItem == newItem
         }
 
+    }
+
+    private fun setCardView(
+        binding: ItemCardGroupScheduleBinding,
+        groupSchedule: GroupScheduleInfo
+    ) {
+        with(binding) {
+            tvGroupName.text = groupSchedule.title
+            tvGroupPlace.text = groupSchedule.location
+            tvGroupTime.text = formatDateTime(groupSchedule.startTime)
+            if(!groupSchedule.isAlarmEnabled) sivNotification.setImageResource(R.drawable.baseline_notifications_off_24)
+            checkBoxParticipation.isChecked = groupSchedule.isAlarmEnabled
+        }
     }
 }
