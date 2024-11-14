48b9208897ffc0cf282959f9134f2c4d1321abd8
Dongsu Lee <dongsu0717@nate.com>
2024-11-14 18:13:05 +0900

    [feat]: FCM왔을때 채널별 울림 알람 추가

--- timely/src/main/java/com/dongsu/timely/TimelyFirebaseMessagingService.kt
+++ timely/src/main/java/com/dongsu/timely/TimelyFirebaseMessagingService.kt
@@ -6,38 +6,59 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import androidx.work.workDataOf
+import com.dongsu.timely.common.CHANNEL_ID
 import com.dongsu.timely.common.GROUP_ID
 import com.dongsu.timely.common.GROUP_NAME
+import com.dongsu.timely.common.SCHEDULE_ID
+import com.dongsu.timely.common.SCHEDULE_START_TIME
+import com.dongsu.timely.common.SCHEDULE_TITLE
 import com.dongsu.timely.presentation.ui.main.TimelyActivity
+import com.dongsu.timely.service.common.FCM_APPOINTMENT_BODY
 import com.dongsu.timely.service.common.FCM_APPOINTMENT_CHANNEL_ID
 import com.dongsu.timely.service.common.FCM_APPOINTMENT_CHANNEL_NAME
 import com.dongsu.timely.service.common.FCM_CREATE_SCHEDULE_BODY
 import com.dongsu.timely.service.common.FCM_CREATE_SCHEDULE_CHANNEL_ID
 import com.dongsu.timely.service.common.FCM_CREATE_SCHEDULE_CHANNEL_NAME
+import com.dongsu.timely.service.common.NOTIFICATION_FCM_APPOINTMENT_ALARM_ID
+import com.dongsu.timely.service.common.NOTIFICATION_FCM_CREATE_SCHEDULE_ID
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class TimelyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        Log.e("서버에서 온 fcm", message.data.toString())
-        val groupId = message.data["groupId"]?.toInt()
-        val groupName = message.data["groupName"].toString()
-        val scheduleTitle = message.data["scheduleTitle"].toString()
-        val scheduleStartTime = message.data["scheduleStartTime"].toString()
-        val channelId = message.data["channelId"].toString()
 
+        Log.e("서버에서 온 fcm", message.data.toString())
+
+        val groupId = message.data[GROUP_ID]?.toInt()
+        val groupName = message.data[GROUP_NAME].toString()
+        val scheduleTitle = message.data[SCHEDULE_TITLE].toString()
+        val scheduleStartTime = message.data[SCHEDULE_START_TIME].toString()
+        val channelId = message.data[CHANNEL_ID].toString()
         groupId?.let {
             when (channelId) {
                 FCM_CREATE_SCHEDULE_CHANNEL_ID -> {
+                    Log.e("FCM일정 만들어짐","FCM일정만들어짐")
                     createSchedulePushMessage(groupId, groupName, scheduleTitle)
                 }
-
                 FCM_APPOINTMENT_CHANNEL_ID -> {
-                    createAppointmentPushMessage(groupId, groupName, scheduleTitle, scheduleStartTime)
+                    Log.e("FCM일정 시간 됨","FCM일정 시간 됐음")
+//                    setWorkManager(message)
+                    AppointmentAlarmPushMessage(groupName, scheduleTitle)
                 }
             }
+            setWorkManager(message)
         }
     }
 
@@ -46,11 +67,14 @@ class TimelyFirebaseMessagingService : FirebaseMessagingService() {
         createChannel(notificationManager,FCM_CREATE_SCHEDULE_CHANNEL_ID, FCM_CREATE_SCHEDULE_CHANNEL_NAME)
         val pendingIntent = createPendingIntent(groupId,groupName)
         val notification = createNotification(pendingIntent,groupName,scheduleTitle)
-        notificationManager.notify(groupId, notification)
+        notificationManager.notify(NOTIFICATION_FCM_CREATE_SCHEDULE_ID, notification)
     }
-    private fun createAppointmentPushMessage(groupId: Int, groupName: String, scheduleTitle: String, scheduleStartTime: String) {
+
+    private fun AppointmentAlarmPushMessage(groupName: String, scheduleTitle: String) {
         val notificationManager = createNotificationManager()
         createChannel(notificationManager, FCM_APPOINTMENT_CHANNEL_ID, FCM_APPOINTMENT_CHANNEL_NAME)
+        val notification = createAppointmentNotification(groupName,scheduleTitle)
+        notificationManager.notify(NOTIFICATION_FCM_APPOINTMENT_ALARM_ID, notification)
     }
 
     private fun createNotificationManager(): NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
@@ -63,7 +87,6 @@ class TimelyFirebaseMessagingService : FirebaseMessagingService() {
         val target = Intent(this, TimelyActivity::class.java).apply {
             putExtra(GROUP_ID, groupId)
             putExtra(GROUP_NAME, groupName)
-//            putExtra(GROUP_SCHEDULE, GROUP_SCHEDULE)
         }
         return PendingIntent.getActivity(
             this,
@@ -80,7 +103,13 @@ class TimelyFirebaseMessagingService : FirebaseMessagingService() {
         .setAutoCancel(true)
         .setContentIntent(pendingIntent)
         .build()
-
+    private fun createAppointmentNotification(groupName: String, scheduleTitle: String)
+            = NotificationCompat.Builder(this, FCM_CREATE_SCHEDULE_CHANNEL_ID)
+        .setSmallIcon(com.dongsu.service.R.drawable.baseline_notifications_active_24)
+        .setContentTitle(groupName)
+        .setContentText(String.format(Locale.getDefault(), FCM_APPOINTMENT_BODY, scheduleTitle))
+        .setAutoCancel(true)
+        .build()
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
