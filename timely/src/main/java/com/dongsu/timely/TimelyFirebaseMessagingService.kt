57afc70de42cf39ac36df192ad54ee89220e91c4
Dongsu Lee <dongsu0717@nate.com>
2024-11-14 18:12:14 +0900

    [feat]:FCM왔을때 WorkManager 세팅 추가

--- timely/src/main/java/com/dongsu/timely/TimelyFirebaseMessagingService.kt
+++ timely/src/main/java/com/dongsu/timely/TimelyFirebaseMessagingService.kt
@@ -85,4 +85,37 @@ class TimelyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
     }
+
+    private fun setWorkManager(message: RemoteMessage){
+        Log.e("워크매니저 세팅","워크매니저세팅부분호출")
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val workData = workDataOf(
+            GROUP_ID to message.data[GROUP_ID]?.toInt(),
+            GROUP_NAME to message.data[GROUP_NAME].toString(),
+            SCHEDULE_TITLE to message.data[SCHEDULE_TITLE].toString(),
+            SCHEDULE_START_TIME to message.data[SCHEDULE_START_TIME].toString(),
+            CHANNEL_ID to message.data[CHANNEL_ID].toString(),
+            SCHEDULE_ID to message.data[SCHEDULE_ID]?.toInt()
+        )
+        val scheduleStartTimeStr = message.data[SCHEDULE_START_TIME].toString()
+        val initialDelay = calculateInitialDelay(scheduleStartTimeStr, 30)
+        val workRequest: WorkRequest = OneTimeWorkRequestBuilder<LocationWorker>()
+            .setConstraints(constraints)
+//            .setInitialDelay(initialDelay, TimeUnit.MINUTES) //실제 약속시간시간 30분전에 울리는거
+            .setInitialDelay(15,TimeUnit.SECONDS) //실험용 15초뒤에 작동 시키기
+            .setInputData(workData)
+            .build()
+        WorkManager.getInstance(applicationContext).enqueue(workRequest)
+    }
+    private fun calculateInitialDelay(startTimeStr: String, minutesBefore: Long): Long {
+        val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+        val scheduleStartTime = LocalDateTime.parse(startTimeStr, formatter)
+        val currentTime = LocalDateTime.now()
+
+        val delayDuration = Duration.between(currentTime, scheduleStartTime.minusMinutes(minutesBefore))
+        return if (delayDuration.isNegative) 0 else delayDuration.toMinutes()
+    }
 }
