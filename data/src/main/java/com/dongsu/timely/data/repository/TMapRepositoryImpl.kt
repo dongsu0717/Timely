e8a786d6af19d427fe40bdac13d6b482583fef5f
Dongsu Lee <dongsu0717@nate.com>
2024-11-16 20:22:13 +0900

    [chore]: Tmap -> TMap 이름변경

--- data/src/main/java/com/dongsu/timely/data/repository/TMapRepositoryImpl.kt
+++ data/src/main/java/com/dongsu/timely/data/repository/TMapRepositoryImpl.kt
@@ -2,13 +2,13 @@ package com.dongsu.timely.data.repository
 
 import com.dongsu.timely.common.TimelyResult
 import com.dongsu.timely.data.mapper.PoiLocationMapper
-import com.dongsu.timely.data.remote.api.TmapService
+import com.dongsu.timely.data.remote.api.TMapService
 import com.dongsu.timely.domain.model.PoiItem
 import com.dongsu.timely.domain.repository.TMapRepository
 import javax.inject.Inject
 
 class TMapRepositoryImpl @Inject constructor(
-    private val tmapService: TmapService,
+    private val tmapService: TMapService,
 //    private val userLocationManager: UserLocationManager
 ) : TMapRepository {
 
