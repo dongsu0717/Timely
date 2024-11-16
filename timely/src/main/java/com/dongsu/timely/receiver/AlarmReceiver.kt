5eacc3593eb30a5ed0de66f06c166b9b082b0e74
Dongsu Lee <dongsu0717@nate.com>
2024-11-16 20:21:06 +0900

    [feat]: 개인스케줄 위치 있을시 거리,시간표시 및 TMap연결

--- timely/src/main/java/com/dongsu/timely/receiver/AlarmReceiver.kt
+++ timely/src/main/java/com/dongsu/timely/receiver/AlarmReceiver.kt
@@ -20,36 +20,48 @@ import com.dongsu.timely.common.PERSONAL_CHANNEL_NAME
 import com.dongsu.timely.common.PERSONAL_ROUTE_CHANNEL_DESCRIPTION
 import com.dongsu.timely.common.PERSONAL_ROUTE_CHANNEL_ID
 import com.dongsu.timely.common.PERSONAL_ROUTE_CHANNEL_NAME
+import com.dongsu.timely.common.TIME_TO_ROUTE_SCHEDULE
 import com.dongsu.timely.common.TIME_TO_SCHEDULE
 import com.dongsu.timely.common.TMAP_MARKET_URL
 import com.dongsu.timely.common.TMAP_PACKAGE_NAME
 import com.dongsu.timely.common.TMAP_ROUTE_URL
+import com.dongsu.timely.common.TimelyResult
+import com.dongsu.timely.data.local.fusedlocation.LocationReceiver
+import com.dongsu.timely.data.remote.api.TMapService
 import com.dongsu.timely.domain.model.ScheduleDistanceTime
 import com.dongsu.timely.domain.model.UserLocation
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import java.util.Locale
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AlarmReceiver : BroadcastReceiver() {
+    @Inject lateinit var locationReceiver: LocationReceiver
+    @Inject lateinit var tMapService: TMapService
 
     override fun onReceive(context: Context, intent: Intent) {
-        val (scheduleTitle, appointmentPlace, destinationLatitude, destinationLongitude, alarmTime) = extractAlarmData(
-            intent
-        ) //구조 분해 선언 처음 적용
+        val (scheduleTitle, appointmentPlace, destinationLatitude, destinationLongitude, alarmTime)
+        = extractAlarmData(intent)
         Log.e("약속 장소 위치", "latitude: $destinationLatitude, longitude: $destinationLongitude")
-
         if (destinationLatitude != 0.0 || destinationLongitude != 0.0) { //일정 장소 있으면 거리,시간 계산
-            createRouteNotification(
-                context,
-                scheduleTitle,
-                appointmentPlace,
-                destinationLatitude,
-                destinationLongitude
-            )
+            CoroutineScope(Dispatchers.IO).launch {
+                createRouteNotification(
+                    context,
+                    scheduleTitle,
+                    appointmentPlace,
+                    destinationLatitude,
+                    destinationLongitude
+                )
+            }
         } else createNotification(context, scheduleTitle, alarmTime)
     }
 
     private fun createNotification(context: Context, scheduleTitle: String, alarmTime: Int) {
+        Log.e("기본알람","start")
         val notificationManager = NotificationUtils.createNotificationManager(context)
         NotificationUtils.createNotificationChannel(
             notificationManager,
@@ -67,13 +79,14 @@ class AlarmReceiver : BroadcastReceiver() {
         notificationManager.notify(NOTIFICATION_SCHEDULE_ID, notification)
     }
 
-    private fun createRouteNotification(
+    private suspend fun createRouteNotification(
         context: Context,
         scheduleTitle: String,
         appointmentPlace: String,
         destinationLatitude: Double,
         destinationLongitude: Double
     ) {
+        Log.e("기본 루트 알람","start")
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         val channel = NotificationChannel(
@@ -88,50 +101,36 @@ class AlarmReceiver : BroadcastReceiver() {
             vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
         }
         notificationManager.createNotificationChannel(channel)
-//        val (startLatitude, startLongitude) = getCurrentLocation(context)
-//        val (distance, carTime, walkTime) = getDistanceAndTime(startLatitude, startLongitude, destinationLatitude, destinationLongitude)
-
-//        val pendingIntent = createTMapPendingIntent(context, appointmentPlace, destinationLatitude, destinationLongitude, startLatitude, startLongitude)
-//
-//        val formattedDistance = formatDistance(distance)
-//        val formattedCarTime = formatTime(carTime)
-//        val formattedWalkTime = formatTime(walkTime)
+        val (startLatitude, startLongitude) = getCurrentLocation()
+        val (distance, carTime, walkTime) = getDistanceAndTime(startLatitude, startLongitude, destinationLatitude, destinationLongitude)
+        val pendingIntent = createTMapPendingIntent(context, appointmentPlace, destinationLatitude, destinationLongitude, startLatitude, startLongitude)
+        val formattedDistance = formatDistance(distance)
+        val formattedCarTime = formatTime(carTime)
+        val formattedWalkTime = formatTime(walkTime)
 
         val notification = NotificationCompat.Builder(context, PERSONAL_ROUTE_CHANNEL_ID)
             .setContentTitle(scheduleTitle)
-//            .setContentText(String.format(Locale.getDefault(), TIME_TO_ROUTE_SCHEDULE,formattedDistance,formattedCarTime,formattedWalkTime))
-//            .setSmallIcon(R.drawable.baseline_notifications_active_24)
+            .setContentText(String.format(Locale.getDefault(), TIME_TO_ROUTE_SCHEDULE,formattedDistance,formattedCarTime,formattedWalkTime))
+            .setSmallIcon(R.drawable.baseline_notifications_active_24)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
-//            .setContentIntent(pendingIntent)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager.notify(NOTIFICATION_ROUTE_ID, notification)
     }
 
-    private fun getCurrentLocation(context: Context): UserLocation {
-        var startLatitude = 0.0
-        var startLongitude = 0.0
-//        userLocationManager.getCurrentLocation() //이게 실행되는 순간 getDistanceAndTime도 실행된다..
-//        userLocationManager.currentLocation.also { location ->
-//            when (location) {
-//                is TimelyResult.Success -> {
-//                    startLatitude = location.resultData.latitude
-//                    startLongitude = location.resultData.longitude
-//                    Log.e("내위치 가져옴", "위도: $startLatitude, 경도: $startLongitude")
-//                    userLocationManager.stopLocationUpdates()
-//                }
-//                is TimelyResult.Loading -> {
-//                    showToast(context,LOADING_GET_LOCATION) //나중에 삭제할 애들
-//                    Log.e("위치 정보", LOADING_GET_LOCATION)
-//                }
-//                is TimelyResult.Error -> {
-//                    showToast(context,ERROR_GET_LOCATION) //나중에 삭제할 애들
-//                    Log.e("위치 정보",ERROR_GET_LOCATION)
-//                }
-//                else ->{}
-//            }
-//        }
-        return UserLocation(startLatitude, startLongitude)
+    private suspend fun getCurrentLocation(): UserLocation {
+        var userLocation = UserLocation(0.0, 0.0)
+        locationReceiver.startReceivingLocation(-1)
+        try {
+            val result = locationReceiver.myLocation.first { it is TimelyResult.Success }
+            if (result is TimelyResult.Success) {
+                userLocation = UserLocation(result.resultData.locationLatitude, result.resultData.locationLongitude)
+            }
+        } catch (e: Exception) {
+            Log.e("위치 정보", "가져오기 실패: ${e.message}")
+        }
+        return userLocation
     }
 
     private suspend fun getDistanceAndTime(
@@ -143,23 +142,31 @@ class AlarmReceiver : BroadcastReceiver() {
         var distance: Long = 0
         var carTime: Long = 0
         var walkTime: Long = 0
-//        tMapService.getCarResult(startX = startLongitude, startY = startLatitude, endX = destinationLongitude, endY = destinationLatitude).let {
-//            if (it.isSuccessful) {
-//                distance = it.body()!!.features.first().properties.totalDistance
-//                carTime = it.body()!!.features.first().properties.totalTime
-//            } else {
-//                Log.e("실패",it.message())
-//                Log.e("실패",it.errorBody().toString())
-//            }
-//        }
-//        tMapService.getWalkResult(startX = startLongitude, startY = startLatitude, endX = destinationLongitude, endY = destinationLatitude).let {
-//            if (it.isSuccessful) {
-//                walkTime = it.body()!!.features.first().properties.totalTime
-//            } else {
-//                Log.e("실패",it.message())
-//                Log.e("실패",it.errorBody().toString())
-//            }
-//        }
+        tMapService.getCarResult(startX = startLongitude, startY = startLatitude, endX = destinationLongitude, endY = destinationLatitude).let {
+            try {
+                if (it.isSuccessful) {
+                    distance = it.body()!!.features.first().properties.totalDistance
+                    carTime = it.body()!!.features.first().properties.totalTime
+                } else {
+                    Log.e("tamp 차정보 가져오기 실패",it.message())
+                    Log.e("tamp 차정보 가져오기 실패",it.errorBody().toString())
+                }
+            } catch (e: Exception) {
+                Log.e("tmap 차정보 가져오기 에러", e.message.toString())
+            }
+        }
+        tMapService.getWalkResult(startX = startLongitude, startY = startLatitude, endX = destinationLongitude, endY = destinationLatitude).let {
+            try {
+                if (it.isSuccessful) {
+                    walkTime = it.body()!!.features.first().properties.totalTime
+                } else {
+                    Log.e("tamp 걸어서정보 가져오기 실패",it.message())
+                    Log.e("tamp 걸어서정보 가져오기 실패",it.errorBody().toString())
+                }
+            } catch (e: Exception) {
+                Log.e("tmap 걸어서 정보 가져오기 에러", e.message.toString())
+            }
+        }
         return ScheduleDistanceTime(distance, carTime, walkTime)
     }
 
@@ -196,7 +203,6 @@ class AlarmReceiver : BroadcastReceiver() {
         } else {
             Intent(Intent.ACTION_VIEW, Uri.parse(String.format(TMAP_MARKET_URL, TMAP_ROUTE_URL)))
         }
-
         return PendingIntent.getActivity(
             context,
             0,
